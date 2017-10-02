ff43616129236dd1caa6c43b1ff68ed62516ff47
andreas <andreas.mittelberger@univie.ac.at>
2017-10-02 13:01:53 +0200

    Fixed bug in change mode function

--- PyLaser/LaserGUI.py
+++ PyLaser/LaserGUI.py
@@ -39,7 +39,7 @@ class LaserGUI(object):
         # Spacing definitions
         default_padx = 5
         default_pady = 3
-        
+
         def connect_button_clicked():
             info_label['text'] = ''
             if self.connect_button['text'] == 'Connect to plotter':
@@ -53,10 +53,10 @@ class LaserGUI(object):
             elif self.connect_button['text'] == 'Disconnect':
                 LaserDriver.close()
                 self.info_label['text'] = 'Connect to plotter'
-                
+
         def settings_button_clicked():
             self.info_label['text'] = ''
-            
+
             def resolution_changed(*args):
                 if len(resolution.get()) > 0 and resolution.get() != str(LaserDriver.resolution):
                     try:
@@ -66,11 +66,11 @@ class LaserGUI(object):
                         resolution.set(LaserDriver.resolution)
                     else:
                         LaserDriver.resolution = res
-                        
+
             def serial_port_changed(*args):
                 if len(serial_port.get()) > 0 and serial_port.get() != LaserDriver.arduino_serial_port:
                     LaserDriver.arduino_serial_port = serial_port.get()
-            
+
             def serial_baudrate_changed(*args):
                 if len(serial_baudrate.get()) > 0 and serial_baudrate.get() != str(LaserDriver.arduino_serial_baudrate):
                     try:
@@ -80,7 +80,7 @@ class LaserGUI(object):
                         serial_baudrate.set(LaserDriver.arduino_serial_baudrate)
                     else:
                         LaserDriver.arduino_serial_port = res
-            
+
             def steps_x_per_mm_changed(*args):
                 if len(x_steps_per_mm.get()) > 0 and x_steps_per_mm.get() != str(LaserDriver.x_steps_per_mm):
                     try:
@@ -90,7 +90,7 @@ class LaserGUI(object):
                         x_steps_per_mm.set(LaserDriver.x_steps_per_mm)
                     else:
                         LaserDriver.x_steps_per_mm = res
-            
+
             def steps_y_per_mm_changed(*args):
                 if len(y_steps_per_mm.get()) > 0 and y_steps_per_mm.get() != str(LaserDriver.y_steps_per_mm):
                     try:
@@ -100,7 +100,7 @@ class LaserGUI(object):
                         y_steps_per_mm.set(LaserDriver.y_steps_per_mm)
                     else:
                         LaserDriver.y_steps_per_mm = res
-            
+
             def x_speed_changed(*args):
                 if len(x_speed.get()) > 0 and x_speed.get() != str(LaserDriver.x_speed):
                     try:
@@ -110,7 +110,7 @@ class LaserGUI(object):
                         x_speed.set(LaserDriver.x_speed)
                     else:
                         LaserDriver.x_speed = res
-            
+
             def y_speed_changed(*args):
                 if len(y_speed.get()) > 0 and y_speed.get() != str(LaserDriver.y_speed):
                     try:
@@ -120,7 +120,7 @@ class LaserGUI(object):
                         y_speed.set(LaserDriver.y_speed)
                     else:
                         LaserDriver.y_speed = res
-                        
+
             settings_window = tk.Toplevel(self.root)
 
             resolution_label = tk.Label(settings_window, font=default_font, text='Resolution (dpi):', anchor=tk.W)
@@ -146,7 +146,7 @@ class LaserGUI(object):
             serial_baudrate.trace('w', serial_baudrate_changed)
             serial_baudrate_field = tk.Entry(settings_window, font=default_font, textvariable=serial_baudrate, width=8)
             serial_baudrate_field.grid(column=1, row=2, columnspan=2, padx=default_padx, pady=default_pady, sticky=tk.W)
-            
+
             steps_per_mm_label = tk.Label(settings_window, font=default_font, text='Steps per mm (x, y):', anchor=tk.W)
             steps_per_mm_label.grid(column=0, row=3, pady=default_pady, padx=default_padx, sticky=tk.W)
             x_steps_per_mm = tk.StringVar()
@@ -159,7 +159,7 @@ class LaserGUI(object):
             x_steps_per_mm_field.grid(column=1, row=3, padx=default_padx, pady=default_pady, sticky=tk.W)
             y_steps_per_mm_field = tk.Entry(settings_window, font=default_font, textvariable=y_steps_per_mm, width=6)
             y_steps_per_mm_field.grid(column=2, row=3, padx=default_padx, pady=default_pady, sticky=tk.W)
-            
+
             speed_label = tk.Label(settings_window, font=default_font, text='Speed in mm/s (x, y):', anchor=tk.W)
             speed_label.grid(column=0, row=4, pady=default_pady, padx=default_padx, sticky=tk.W)
             x_speed = tk.StringVar()
@@ -186,9 +186,13 @@ class LaserGUI(object):
                     return
                 self.gcodefile = filename
                 current_file_name['text'] = self.gcodefile
-        
+
         def start_button_clicked():
             info_label['text'] = ''
+            if self.connect_button['text'] == 'Connect to plotter':
+                info_label['text'] = 'You have to connect to the plotter before starting a plot'
+                return
+
             if self.mode.get() == 'file':
                 try:
                     self._file = open(self.gcodefile)
@@ -204,11 +208,11 @@ class LaserGUI(object):
             elif self.mode.get() == 'raw':
                 self._thread = threading.Thread(target=self.process_raw, args=(raw_entry.get(),))
                 self._thread.start()
-                
+
             self.abort_button.config(state=tk.NORMAL)
             self.start_button.config(state=tk.DISABLED)
             self.connect_button.config(state=tk.DISABLED)
-                
+
         def abort_button_clicked():
             self._abort_move = True
             self.start_button['text'] = 'Start plot'
@@ -218,7 +222,7 @@ class LaserGUI(object):
             self.abort_button.config(state=tk.DISABLED)
             if self._thread is None or not self._thread.is_alive():
                 self._abort_move = False
-        
+
         def mode_changed(*args):
             self.info_label['text'] = ''
             current_file_text.grid_remove()
@@ -238,16 +242,20 @@ class LaserGUI(object):
             elif self.mode.get() == 'raw':
                 raw_descriptor.grid()
                 raw_entry.grid()
-        
+
         def simulate_button_clicked():
             self.do_simulation = True
             start_button_clicked()
-        
+
         def clear_button_clicked():
             self.simulator_canvas.delete(tk.ALL)
             LaserDriver.current_steps_x = 0
             LaserDriver.current_steps_y = 0
-            
+
+        #info label
+        info_label = tk.Label(self.root, font=default_font, anchor=tk.W)
+        info_label.grid(column=0, row=3, columnspan=3, padx=default_padx, pady=default_pady, sticky=tk.W)
+        self.info_label = info_label
         # Elements for "file" mode
         current_file_text = tk.Label(self.root, text='Current file:', font=default_font, anchor=tk.W)
         current_file_text.grid(column=0, row=1, padx=default_padx, pady=default_pady, sticky=tk.W)
@@ -282,10 +290,6 @@ class LaserGUI(object):
         mode_combo_box = tk.OptionMenu(self.root, self.mode, *mode_options)
         mode_combo_box.config(font=default_font)
         mode_combo_box.grid(column=0, row=2, padx=default_padx, pady=default_pady)
-        #info label
-        info_label = tk.Label(self.root, font=default_font, anchor=tk.W)
-        info_label.grid(column=0, row=3, columnspan=3, padx=default_padx, pady=default_pady, sticky=tk.W)
-        self.info_label = info_label
         #simulator canvas
         self.simulator_canvas = tk.Canvas(self.root, height=200, width=200, bg='white')
         self.simulator_canvas.grid(column=4, row=1, rowspan=3, columnspan=2)
@@ -294,14 +298,14 @@ class LaserGUI(object):
         clear_button = tk.Button(self.root, text='Clear', command=clear_button_clicked, font=default_font)
         clear_button.grid(column=5, row=0, padx=default_padx, pady=default_pady)
         #oval = self.simulator_canvas.create_oval(10, 30, 40, 50)
-        
-        
+
+
         self.root.mainloop()
-        
+
     def process_file(self):
         if self._current_line is not None:
             self.process_line(self._current_line)
-            
+
         for line in self._file:
             if self._abort_move:
                 self.steps = None
@@ -313,11 +317,11 @@ class LaserGUI(object):
             try:
                 self.process_line(line)
             except RuntimeError:
-                self.finish()                
+                self.finish()
                 return
         self._current_line = None
         self.finish()
-    
+
     def process_line(self, line):
         self.info_label['text'] = ''
         line = line.upper()
@@ -325,7 +329,7 @@ class LaserGUI(object):
         if line.startswith('G') and not self.do_simulation:
             LaserDriver.current_steps_x = LaserDriver.get_current_steps('x')
             LaserDriver.current_steps_y = LaserDriver.get_current_steps('y')
-        
+
         if line.startswith('G00'):
             position = LaserDriver.parse_line(line)
             self.steps = LaserDriver.move_linear(position, engrave=False)
@@ -343,7 +347,7 @@ class LaserGUI(object):
             pass
         else:
             print('unrecognized command')
-        
+
         try:
             if self.do_simulation:
                 self.execute_simulation_move()
@@ -354,12 +358,12 @@ class LaserGUI(object):
         finally:
             if self.mode.get() == 'line':
                 self.finish()
-    
+
     def process_raw(self, raw):
         res = LaserDriver.send_raw(raw)
         self.info_label['text'] = res
         self.finish()
-    
+
     def execute_move(self):
         if self.steps is not None:
             try:
@@ -376,7 +380,7 @@ class LaserGUI(object):
                 self._current_counter = 0
                 self.start_button['text'] = 'Start plot'
                 self.start_button.config(state=tk.NORMAL)
-    
+
     def execute_simulation_move(self):
         min_x = min_y = np.inf
         max_x = max_y = -np.inf
@@ -394,7 +398,7 @@ class LaserGUI(object):
                         max_y = step_mm
                     if step_mm < min_y:
                         min_y = step_mm
-                        
+
             last_x = LaserDriver.current_steps_x
             last_y = LaserDriver.current_steps_y
             counter = 0
@@ -423,7 +427,7 @@ class LaserGUI(object):
         self.connect_button.config(state=tk.NORMAL)
         self.do_simulation = False
         self._abort_move = False
-                
+
 if __name__ == '__main__':
-    GUI = LaserGUI()    
+    GUI = LaserGUI()
     GUI.create_gui()
