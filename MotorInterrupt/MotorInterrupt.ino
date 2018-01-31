fda4ea4703f2f51ff58231489993ee63646b78be
Andi <Brow71189@gmail.com>
2018-02-01 00:25:13 +0100

    Finished calibration function

--- MotorInterrupt/MotorInterrupt.ino
+++ MotorInterrupt/MotorInterrupt.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 volatile long counterA = 0;
 volatile long counterB = 0;
 long target = 0;
@@ -7,6 +9,7 @@ float speedA = 10.0*378.21; // in steps per second
 float speedB = 10.0*11.71; // in steps per second
 byte PWMA = 127;
 byte PWMB = 127;
+const byte PWMTolerance = 20;
 volatile unsigned long last_timeA = 0;
 volatile unsigned long last_timeB = 0;
 volatile unsigned long this_timeA = 0;
@@ -27,6 +30,11 @@ const byte SensorPinA = 4;
 const byte SensorPinB = 5;
 byte verbosity = 1; //0: Only send necessary status messages, 1: Also send information, 2: show debug info
 short last_direction = 0;
+float slopeA = 0;
+float slopeB = 0;
+float offsetA;
+float offsetB;
+const byte magic_number = 42;
 
 void setup()
 {
@@ -45,6 +53,7 @@ void setup()
   }
   attachInterrupt(digitalPinToInterrupt(InterruptPinA), countA, RISING);
   attachInterrupt(digitalPinToInterrupt(InterruptPinB), countB, CHANGE);
+  load_from_EEPROM();
   Serial.begin(115200);
   Serial.setTimeout(100);
 }
@@ -60,7 +69,8 @@ void loop()
     }
   }
 }
-  
+// X: move motor, V: set verbosity, R: ready check, P: get position, L: set laser state (for movements),
+// F: switch laser on/off (immediately), C: set counter, S: set speed, D: do speed calibration   
 void process_line() {
   char cmd = Serial.read();
   char motor_id;
@@ -95,32 +105,33 @@ void process_line() {
                 Serial.write('F');
                 return;
             
-      case 'C': while (!Serial.available()) {
+     case 'C': while (!Serial.available()) {
     		        delayMicroseconds(5);
     	          }
-    			  motor_id = Serial.read();
-    			  switch (motor_id) {
-    				  case 'A': counterA = Serial.parseInt(); Serial.write('C'); break;
-    				  case 'B': counterB = Serial.parseInt(); Serial.write('C'); break;
-    				  default: if (verbosity > 0) {
-    					         Serial.print("Invalid motor ID: "); Serial.println(motor_id);
-    				           }
-    						   Serial.write('E'); break;
+    			    motor_id = Serial.read();
+    			    switch (motor_id) {
+    				      case 'A': counterA = Serial.parseInt(); Serial.write('C'); break;
+    				      case 'B': counterB = Serial.parseInt(); Serial.write('C'); break;
+    				      default: if (verbosity > 0) {
+    					                Serial.print("Invalid motor ID: "); Serial.println(motor_id);
+    				                }
+    						            Serial.write('E'); break;
     			  }
            return;
-    	 case 'S': while (!Serial.available()) {
+     case 'S': while (!Serial.available()) {
     		        delayMicroseconds(5);
-    	          }
-    			  char motor_id = Serial.read();
-    			  switch (motor_id) {
-    				  case 'A': speedA = Serial.parseFloat(); Serial.write('S'); break;
-    				  case 'B': speedB = Serial.parseFloat(); Serial.write('S'); break;
-    				  default: if (verbosity > 0) {
-    					         Serial.print("Invalid motor ID: "); Serial.println(motor_id);
-    				           }
-    						   Serial.write('E'); break;
-    			  }
-           return;
+    	         }
+    			     motor_id = Serial.read();
+    			     switch (motor_id) {
+    				      case 'A': speedA = Serial.parseFloat(); Serial.write('S'); break;
+    				      case 'B': speedB = Serial.parseFloat(); Serial.write('S'); break;
+    				      default: if (verbosity > 0) {
+    					             Serial.print("Invalid motor ID: "); Serial.println(motor_id);
+    				               }
+    						           Serial.write('E'); break;
+    			      }
+                return;
+      case 'D': char res = calibrate_speeds(); Serial.write(res); return;
   }
   if (verbosity > 0) {
     Serial.print(cmd);
@@ -146,20 +157,43 @@ char move_to(char motor_id, long* target_pos) {
   int blockedThreshold;
   volatile unsigned long* last_time;
   volatile unsigned long* this_time;
+  bool has_calibration = false;
   switch (motor_id) {
-      case 'A': counter = &counterA; MotorPin1 = MotorAPin1; MotorPin2 = MotorAPin2; PWMPin = PWMPinA;
-                MaxPWMValue = 255; MinPWMValue = 0; blockedThreshold = 300; target_speed = speedA; PWMValue = &PWMA;
-                last_time = &last_timeA; this_time = &this_timeA; backlashSteps = 150; break;
-      case 'B': counter = &counterB; MotorPin1 = MotorBPin1; MotorPin2 = MotorBPin2; PWMPin = PWMPinB;
-                MaxPWMValue = 255; MinPWMValue = 0; blockedThreshold = 24; target_speed = speedB; PWMValue = &PWMB;
-                last_time = &last_timeB; this_time = &this_timeB; backlashSteps = 0; break;
+      case 'A': if (slopeA > 0) {
+                  PWMA = (byte)round(speedA*slopeA + offsetA);
+                  has_calibration = true;
+                }
+                PWMValue = &PWMA;
+                counter = &counterA; MotorPin1 = MotorAPin1; MotorPin2 = MotorAPin2; PWMPin = PWMPinA;
+                blockedThreshold = 300; target_speed = speedA; last_time = &last_timeA;
+                this_time = &this_timeA; backlashSteps = 150; break;
+      case 'B': if (slopeB > 0) {
+                  PWMB = (byte)round(speedB*slopeB + offsetB);
+                  has_calibration = true;
+                }
+                PWMValue = &PWMB;
+                counter = &counterB; MotorPin1 = MotorBPin1; MotorPin2 = MotorBPin2; PWMPin = PWMPinB;
+                blockedThreshold = 24; target_speed = speedB; last_time = &last_timeB;
+                this_time = &this_timeB; backlashSteps = 0; break;
       default: if (verbosity > 0) {
                  Serial.print("Invalid motor ID: "); Serial.println(motor_id);
                }
                return 'E';
     }
-  //float slope = (float)(MinPWMValue - MaxPWMValue) / brakeThreshold;
-  //bool first_move = true;
+  if (*PWMValue < 255-PWMTolerance && has_calibration) {
+    MaxPWMValue = *PWMValue+PWMTolerance;
+  } else {
+    MaxPWMValue = 255;
+  }
+
+  if (*PWMValue > PWMTolerance && has_calibration) {
+    MinPWMValue = *PWMValue-PWMTolerance; 
+  } else {
+    MinPWMValue = 0;
+  }
+  Serial.println(*PWMValue);Serial.println(MaxPWMValue);
+  Serial.println(MinPWMValue);
+  
   float current_speed = target_speed;
   long difference = *counter - *target_pos;
   if (last_direction > 0 && difference < 0) {
@@ -191,13 +225,15 @@ char move_to(char motor_id, long* target_pos) {
 	  difference = current_position - *target_pos;
   	if (last_position != current_position || (now - last_loop_time) > 2.0e6/target_speed ) { // only update speed if counter changed since last time or if more time passed than we would expect for the given speed
       unsigned long time_diff = *this_time - *last_time;
-      current_speed = 1.0 / (float)(time_diff) * 1e6;  
-      
+      if (time_diff > 0) {
+        current_speed = 1.0 / (float)(time_diff) * 1e6;  
+      }
+            
   	  if (verbosity > 1) {
         Serial.print("Current speed: "); Serial.print(current_speed); Serial.write(" "); Serial.print(last_position-current_position); Serial.write(" "); Serial.print(*PWMValue); Serial.write(" "); Serial.print(time_diff); Serial.write(" "); Serial.println(target_speed);
   	  }
   	  if (last_position == current_position) {
-        not_moved += 1;
+        not_moved++;
         if (not_moved > 3) {
           current_speed = 0;
         }
@@ -219,7 +255,7 @@ char move_to(char motor_id, long* target_pos) {
         }
 	  
   	  if ((difference < 0 && (last_position-current_position) > 0) || (difference > 0 && (last_position-current_position) < 0)) {
-        current_speed = 0;
+        current_speed = target_speed + 1;
   	  }
   	
   	  if (current_speed > target_speed && *PWMValue > MinPWMValue) {
@@ -263,11 +299,10 @@ char calibrate_speeds() {
   byte PWMValuesA[5];
   byte PWMValuesB[5];
   float speedsA[] = {speedA/8, speedA/4, speedA/2, speedA, speedA*2};
-  float speedsB[] = {speedA/8, speedB/4, speedB/2, speedB, speedB*2};
-  float slopeA = 0;
-  float slopeB = 0;
-  float offsetA;
-  float offsetB;
+  float speedsB[] = {speedB/8, speedB/4, speedB/2, speedB, speedB*2};
+  slopeA = 0;
+  slopeB = 0;
+  
   target = 0;
   movement_result = move_to('A', &target);
   if (movement_result != 'X') {
@@ -278,22 +313,28 @@ char calibrate_speeds() {
     return movement_result;
   }
   for (byte i=0; i<5; i++) {
-    target = speedsA[i];
+    target = (long)round(speedsA[i]);
+    speedA = speedsA[i];
     movement_result = move_to('A', &target);
     if (movement_result != 'X') {
+      speedA = speedsA[3];
       return movement_result;
     }
     PWMValuesA[i] = PWMA;
   }
+  speedA = speedsA[3];
 
   for (byte i=0; i<5; i++) {
-    target = speedsB[i];
+    target = (long)round(speedsB[i]);
+    speedB = speedsB[i];
     movement_result = move_to('B', &target);
     if (movement_result != 'X') {
+      speedB = speedsB[3];
       return movement_result;
     }
     PWMValuesB[i] = PWMB;
   }
+  speedB = speedsB[3];
   //calculate average slope
   for (byte i=0; i<4; i++) {
      slopeA += ((float)(PWMValuesA[i+1] - PWMValuesA[i]) / (speedsA[i+1] - speedsA[i]))/4;
@@ -302,10 +343,48 @@ char calibrate_speeds() {
   //use one point to find offset
   offsetA = (float)PWMValuesA[0] - slopeA*speedsA[0];
   offsetB = (float)PWMValuesB[0] - slopeB*speedsB[0];
-
+  save_to_EEPROM();
+  target = 0;
+  movement_result = move_to('A', &target);
+  movement_result = move_to('B', &target);
+  return 'D';
 }
 
-char calibrate_positions() {
+void save_to_EEPROM() {
+  save_to_EEPROM(0);
+}
+
+void save_to_EEPROM(int start_address) {
+  unsigned int addr = start_address;
+  EEPROM.update(addr, magic_number);
+  addr++;
+  float* to_save[] = {&slopeA, &offsetA, &slopeB, &offsetB};
+  for (byte i=0; i<4; i++) {
+    EEPROM.put(addr, *to_save[i]); 
+    addr += sizeof(float);
+  }
+}
+
+void load_from_EEPROM() {
+  unsigned int addr;
+  for (addr=0; addr < EEPROM.length(); addr++) {
+    if (EEPROM.read(addr) == magic_number) {
+      break;
+    }
+  }
+  addr++;
+  if (addr >= EEPROM.length() - 16) {
+    return;
+  }
+
+  float* to_read[] = {&slopeA, &offsetA, &slopeB, &offsetB};
+  float read_value;
+  for (byte i=0; i<4; i++) {
+    EEPROM.get(addr, read_value);
+    *to_read[i] = read_value;
+    addr += sizeof(float);
+  }
+  
   
 }
 
